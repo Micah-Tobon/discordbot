099efd812400d17badef026a5937fb1e7f530f16
Micah-Tobon <mt405516@ohio.edu>
2019-03-03 12:58:46 -0500

    Removed botinfo.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-var Discord = require('discord.io',"botinfo.js");
+var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
 // Configure logger settings
