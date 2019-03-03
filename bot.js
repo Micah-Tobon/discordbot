ce8d9026b047909bd724decbba7ca740df2ccadd
Micah-Tobon <mt405516@ohio.edu>
2019-03-03 12:26:59 -0500

    Added back message function

--- bot.js
+++ bot.js
@@ -13,8 +13,30 @@ var bot = new Discord.Client({
    autorun: true
 });
 bot.on('ready', function (evt) {
+    logger.info('Connected');
     logger.info('Logged in as: ');
     logger.info(bot.username + ' - (' + bot.id + ')');
+});
+bot.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with !
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+
+        args = args.splice(1);
+        switch(cmd) {
+            // !ping
+            case 'ping':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'I love you daniel!'
+                });
+            break;
+            // Just add any case commands if you want to..
+         }
+     }
+
 });
 
 module.exports.run = async (bot, message, args) => {
